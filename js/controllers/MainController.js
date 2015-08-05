4addc252d7ca98c718dead32fef162db7474300e
Tony DiNitto <think41c@gmail.com>
2015-08-04 23:44:06 -0500

    Added product hash.

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -2,4 +2,9 @@ app.controller('MainController',
   ['$scope', function($scope) {
     $scope.title = 'Tony\'s Favorite Books';
     $scope.promo = "50% off today!";
+    $scope.product = {
+      name: 'The Book of Trees',
+      price: 19
+    }
+    
   }]);
