a223ca28d21db87d07e67d888a3c37dac165c464
Tony DiNitto <think41c@gmail.com>
2015-08-04 23:57:23 -0500

    Added publication date and filtered the display of the date.

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,10 +1,11 @@
-app.controller('MainController',
-  ['$scope', function($scope) {
-    $scope.title = 'Tony\'s Favorite Books';
-    $scope.promo = "50% off today!";
-    $scope.product = {
-      name: 'The Book of Trees',
-      price: 19
-    }
-    
-  }]);
+  app.controller('MainController',
+    ['$scope', function($scope) {
+      $scope.title = 'Tony\'s Favorite Books';
+      $scope.promo = "50% off today!";
+      $scope.product = {
+        name: 'The Book of Trees',
+        price: 19,
+        pubdate: new Date('2014', '03', '08')
+      }
+
+    }]);
