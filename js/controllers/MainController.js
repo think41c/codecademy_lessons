a997395fa07178c4d1a5191b0ef1f89f933b5b67
Tony DiNitto <think41c@gmail.com>
2015-08-05 23:52:36 -0500

    Added new products object.

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -2,10 +2,18 @@
     ['$scope', function($scope) {
       $scope.title = 'Tony\'s Favorite Books';
       $scope.promo = "50% off today!";
-      $scope.product = {
-        name: 'The Book of Trees',
-        price: 19,
-        pubdate: new Date('2014', '03', '08')
-      }
-
+      $scope.products = [ 
+  { 
+    name: 'The Book of Trees', 
+    price: 19, 
+    pubdate: new Date('2014', '03', '08'), 
+    cover: 'img/the-book-of-trees.jpg' 
+  }, 
+  { 
+    name: 'Program or be Programmed', 
+    price: 8, 
+    pubdate: new Date('2013', '08', '01'), 
+    cover: 'img/program-or-be-programmed.jpg' 
+  } 
+]
     }]);
